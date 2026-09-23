9528d7837f32ccdf77c1753ed02036065b0f7a65
vitordanielmarques <estudo.marques1809@gmail.com>
2026-09-23 12:10:09 -0400

    foi

--- multas.js
+++ multas.js
@@ -1,11 +1,14 @@
- function verificar() {
-            // Pega o valor digitado no input
-            var txtv = document.getElementById('txtvel');
-            var res = document.getElementById('resultado');
-            var vel = Number(txtv.value); // Converte para número
+const limiteVelocidade = 80;
 
-            // Exibe a velocidade atual (deixando o valor em negrito, como na imagem)
-            res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}</strong> km/h.</p>`;
+function verificar() {
+            // Pega o valor digitado no input
+            const campo = document.getElementById('velocidade');
+            const velocidade = document.getElementById('resultado');
+            
+            var vel = Number(campo.value); // Converte para número
+
+            // Exibe a velocidade atual (deixando o valor em negrito, como na image)
+            velocidade.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}</strong> km/h.</p>`;
 
             // Lógica para verificar se foi multado (considerando 60 km/h como limite)
             if (vel > 60) { 
