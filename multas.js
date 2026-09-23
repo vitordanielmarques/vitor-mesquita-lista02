7d43adba02766fd1f20cf8e92b773c9bb43f2b88
vitordanielmarques <estudo.marques1809@gmail.com>
2026-09-23 12:15:26 -0400

    Update multas.js

--- multas.js
+++ multas.js
@@ -1,19 +1,19 @@
-const limiteVelocidade = 80;
-
-function verificar() {
-            // Pega o valor digitado no input
-            const campo = document.getElementById('velocidade');
-            const velocidade = document.getElementById('resultado');
-            
-            var vel = Number(campo.value); // Converte para número
-
-            // Exibe a velocidade atual (deixando o valor em negrito, como na image)
-            velocidade.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}</strong> km/h.</p>`;
-
-            // Lógica para verificar se foi multado (considerando 60 km/h como limite)
-            if (vel > 60) { 
-                res.innerHTML += `<p>Velocidade acima do permitido! MULTADO.</p>`;
-            } else {
-                res.innerHTML += `<p>Dirija sempre usando cinto de segurança!</p>`;
-            }
-        }
+//velocidade máxima
+    const LIMITE = 80;
+ 
+    function verificar() {
+      const campo = document.getElementById("velocidade");
+      const velocidade = Number(campo.value);
+ 
+      const mensagemVelocidade = document.getElementById("mensagemVelocidade");
+      const resultado = document.getElementById("resultado");
+ 
+      mensagemVelocidade.innerHTML =
+        "Sua velocidade atual é de <b>" + velocidade + "</b> km/h.";
+ 
+      if (velocidade > LIMITE) {
+        resultado.innerHTML = "Velocidade acima do permitido! MULTADO.";
+      } else {
+        resultado.innerHTML = "Velocidade dentro do limite. Tudo certo!";
+      }
+    }
